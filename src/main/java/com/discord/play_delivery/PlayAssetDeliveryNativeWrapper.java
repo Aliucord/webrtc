25c678642eda8ce72a4cd0873acb201ee8a1c7eb
Kofhisho <contact@alphexo.dev>
2026-06-20 03:50:10 +0100

    feat(krisp): Baseline for future impl

--- src/main/java/com/discord/play_delivery/PlayAssetDeliveryNativeWrapper.java
+++ src/main/java/com/discord/play_delivery/PlayAssetDeliveryNativeWrapper.java
@@ -3,6 +3,13 @@ package com.discord.play_delivery;
 // Stub required by native code
 /** @noinspection unused */
 public class PlayAssetDeliveryNativeWrapper {
-    public static String getKrispAssetPackLocation() { return ""; }
-}
+    private static volatile String krispAssetPackLocation = "";
 
+    public static void setKrispAssetPackLocation(String location) {
+        krispAssetPackLocation = location;
+    }
+
+    public static String getKrispAssetPackLocation() {
+        return krispAssetPackLocation;
+    }
+}
